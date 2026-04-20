bf8931e9203d0800357b36742a0d19d618b3ec1b
ANIN M SADATH <aninsadath@gmail.com>
2026-04-20 03:19:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7067177088-c81e4",
+  "appId": "1:174302856420:web:ebaa0c64973053d84818f1",
+  "apiKey": "AIzaSyA_8kpremmv-G5LvdoWbfI07OCN2n_hzeo",
+  "authDomain": "studio-7067177088-c81e4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "174302856420"
+};
